7b340a0a30185932292c5f2aa8111d674551f89b
John Peele <john@wearegoodcitizen.com>
2014-06-09 12:20:16 -0400

    Responsive css tweaks

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '81c39a98d350f7fc56a15655a197dabd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e7c4f330e959b56320306cfa5d9ee0d9');
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
