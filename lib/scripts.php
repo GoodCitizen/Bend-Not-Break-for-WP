afe2833537d08d5a93e5336831bb949c4656ff4b
John Peele <john@wearegoodcitizen.com>
2014-06-09 12:04:34 -0400

    Various css tweaks

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7f2481ffc2ad77777cc4ee5d64de2dea');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '81c39a98d350f7fc56a15655a197dabd');
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
