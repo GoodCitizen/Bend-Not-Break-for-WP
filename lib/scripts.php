c01677e340ef8ebbda45618305fed2ba41f314ea
John Peele <john@wearegoodcitizen.com>
2014-06-10 08:29:45 -0400

    Add top border to mobile nav-toggle

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e7c4f330e959b56320306cfa5d9ee0d9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '95122aa49909c8990442f60d3909aa83');
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
