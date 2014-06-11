2bb11c1092cb0d2e0c45cf0dc86d1470f2fa5608
John Peele <john@wearegoodcitizen.com>
2014-06-11 13:36:10 -0400

    Switch .touch and .no-touch class styles

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1aa27f8deaf930f29527b17f76cd03c7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '44a5015d9f1da779ec5fdee53f4d4389');
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
