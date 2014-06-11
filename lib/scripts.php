7dfe20525cb29b9c55b296a888034e5e56896628
John Peele <john@wearegoodcitizen.com>
2014-06-11 13:42:08 -0400

    Add bottom margin to flash animation

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8f932859a5a80d6446b138dab884908b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5cfae4d3a1e8abf8a5298563580db4bf');
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
